bbc56322cc3f47786657d39bd306688b81e09de8
Carlos Ferreira <carlos@especializati.com.br>
2018-02-05 09:49:38 -0200

    Tipos de variáveis no PHP

---
+++ 04_variaveis.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = 'Carlos Ferreira';
+
+echo $name;
+echo '<br>';
+
+$name = 'Carlos Ferreira Teste';
+echo $name;
+echo '<br>';
+
+$name = 12.2;
+echo $name;
+echo '<br>';
+
+$name = 123;
+echo $name;
