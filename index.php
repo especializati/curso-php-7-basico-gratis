7e18247a560dc686665bb400aed023462a8bcebb
Carlos Ferreira <carlos@especializati.com.br>
2018-02-12 14:39:11 -0200

    Home Page PHP

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Curso PHP Básico</title>
 
-echo 'Bem vindo(a) ao curso de PHP Básico da EspecializaTi';
+        <!--Favicon-->
+		<link rel="icon" type="image/png" href="https://blog.especializati.com.br/wp-content/themes/especializati/imgs/favicon.png">
+    </head>
+    <body>
+
+        <?php
+        echo 'Bem vindo(a) ao curso de PHP Básico da EspecializaTi';
+        ?>
+
+    </body>
+</html>
