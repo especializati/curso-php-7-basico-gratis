b71a5fab8d151860d53e11e45595ecf13d18e639
Carlos Ferreira <carlos@especializati.com.br>
2018-02-14 10:40:12 -0200

    Functions: array_keys, array_values, count, array_merge

---
+++ 08_manipulacao_arrays.php
@@ -0,0 +1,23 @@
+<?php
+
+$infoCompany = [
+    'name'          => 'EspecializaTi',
+    'founder'       => 'Carlos Ferreira',
+    'year_current'  => 2018,
+];
+
+$infoCompanyTwo = [
+    'courses'       => ['PHP', 'JS', 'Vue JS', 'Laravel'],
+    'total_courses' => 26,
+];
+
+// var_dump(array_keys($infoCompany));
+
+// var_dump(array_values($infoCompany));
+
+// var_dump(count($infoCompany));
+
+// $infoCompany = array_merge($infoCompany, $infoCompanyTwo);
+// echo '<pre>';
+// var_dump($infoCompany);
+
