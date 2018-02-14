99b0178e87642900f5bca13385fa372b2dc91d5f
Carlos Ferreira <carlos@especializati.com.br>
2018-02-14 10:37:24 -0200

    Functions: compact, is_array, in_array

---
+++ 07_manipulacao_arrays.php
@@ -0,0 +1,21 @@
+<?php
+
+// $nomes = array('Carlos', 'EspecializaTi', 'Company');
+// $nomes = ['Carlos', 'EspecializaTi', 'Company'];
+$name = 'Carlos';
+$company = 'EspecializaTi';
+$year = 2018;
+
+$infoCompany = [
+    $name,
+    $company,
+    $year
+];
+
+// $infoCompany = compact('name', 'company', 'year');
+// var_dump($infoCompany);
+
+// var_dump(is_array($infoCompany));
+
+// var_dump(in_array('Carlos', $infoCompany));
+
