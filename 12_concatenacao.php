f2bc65501de1b5505768cec39f80ffe314a88549
Carlos Ferreira <carlos@especializati.com.br>
2018-02-15 18:50:53 -0200

    Concatenar valores em PHP

---
+++ 12_concatenacao.php
@@ -0,0 +1,14 @@
+<?php
+
+$var1 = 'Algo';
+$var2 = 'Outro';
+
+$var3 = $var1 .' '. $var2 . ' '. 12;
+// echo $var3;
+
+$name = 'Carlos Ferreira';
+$company = 'EspecializaTi';
+
+// echo 'O '.$name.' é fundador da Empresa: '.$company;
+// echo "O {$name} é fundador da Empresa: {$company}";
+// echo 'Olá mundo!';
