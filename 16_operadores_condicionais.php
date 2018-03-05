ecb03b05a35b55ecf881436cbbc50035ac73b7ab
Carlos Ferreira <conta@especializati.com.br>
2018-03-05 15:11:24 -0300

    Operadores Condicionais ELSEIF no PHP

---
+++ 16_operadores_condicionais.php
@@ -0,0 +1,12 @@
+<?php
+
+$numberOne = 12;
+
+if ($numberOne == 14)
+    echo 'Quatorze';
+elseif($numberOne == 13)
+    echo 'Treze';
+elseif($numberOne == 12)
+    echo 'Doze';
+else
+    echo 'Não identificado...';
