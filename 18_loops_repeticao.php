17a0fe9e99d9b58aee9874e184453ef41e0e418c
Carlos Ferreira <conta@especializati.com.br>
2018-03-05 15:44:19 -0300

    Loops de repetição no PHP: FOR

---
+++ 18_loops_repeticao.php
@@ -0,0 +1,21 @@
+<?php
+
+$names = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+echo '<hr>';
+
+for ($i = 0; $i<count($names); $i++) {
+    echo $names[$i];
+}
+/**
+for ($i = 10; $i>0; $i--) {
+    echo "{$i} <br>";
+}
+ */
+/**
+for ($i = 0; $i<10; $i++) {
+    echo "{$i} <br>";
+}
+ */
+
+echo '<hr>';
