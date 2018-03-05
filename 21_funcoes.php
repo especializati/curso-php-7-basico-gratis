b450d923128963822f421ad2f9197df581d91573
Carlos Ferreira <conta@especializati.com.br>
2018-03-05 16:37:32 -0300

    Funções no PHP

---
+++ 21_funcoes.php
@@ -0,0 +1,13 @@
+<?php
+
+function testFunction(): String
+{
+    return 'Sou uma função <br>';
+}
+
+echo testFunction();
+echo testFunction();
+echo testFunction();
+echo testFunction();
+echo testFunction();
+echo testFunction();
