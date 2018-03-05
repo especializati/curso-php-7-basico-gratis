2992ac9a5c6ad723a38bae33e629077ee5d7a27b
Carlos Ferreira <conta@especializati.com.br>
2018-03-05 15:05:41 -0300

    Operador condicional ELSE no PHP

---
+++ 15_operadores_condicionais.php
@@ -0,0 +1,18 @@
+<?php
+
+$numberOne = 15;
+$numberTwo = 16;
+
+if ($numberOne == $numberTwo) {
+    echo 'É igual';
+} else {
+    echo 'É diferente!';
+}
+
+echo '<hr>';
+
+if ($numberOne < 20) {
+    echo 'É menor!';
+} else {
+    echo 'É maior!';
+}
