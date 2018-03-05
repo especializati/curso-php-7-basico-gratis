09836e3c4b84af1ed49c31b1fa23008d624f96f9
Carlos Ferreira <conta@especializati.com.br>
2018-03-05 17:41:28 -0300

    Funções recursivas no PHP

---
+++ 24_funcoes.php
@@ -0,0 +1,17 @@
+<?php
+
+// 4 => 4*3*2*1 = 24
+// 2 => 2*1 = 2
+// 3 => 3*2*1 = 6
+
+function factorial(int $number): int
+{
+    if ($number <= 1)
+        $number = $number;
+    else
+        $number *= factorial($number -1);
+
+    return $number;
+}
+
+echo factorial(1);
