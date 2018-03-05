06ed0e641a9e2a2cf64067fd543a849a44a22e1d
Carlos Ferreira <conta@especializati.com.br>
2018-03-05 15:20:29 -0300

    Operador Condicional SWITCH no PHP

---
+++ 17_switch.php
@@ -0,0 +1,33 @@
+<?php
+
+$number = 10;
+
+switch ($number) {
+    case $number == 12:
+        echo 'Doze';    
+    break;
+    case $number == 13:
+        echo 'Treze';    
+    break;
+    case $number == 14:
+        echo 'Quatorze';    
+    break;
+    case $number == 15:
+        echo 'Quinze';    
+    break;
+    default: 
+        echo 'Valor não encontrado';
+    break;
+}
+/*
+if ($number == 12)
+    echo 'Doze';
+elseif ($number == 13)
+    echo 'Treze';
+elseif ($number == 14)
+    echo 'Quatorze';
+elseif ($number == 15)
+    echo 'Quinze';
+else
+    echo 'Não identificado...';
+*/
