34df9ea56672560d9571f260316ae2a173ac2ec7
Carlos Ferreira <conta@especializati.com.br>
2018-03-05 15:53:32 -0300

    Loops de repetição no PHP: foreach

---
+++ 19_loops_repeticao.php
@@ -0,0 +1,17 @@
+<?php
+
+// $names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+$names = [
+    'a' => 'aa',
+    'b' => 'bb',
+    'c' => 'cc',
+    'd' => 'dd',
+    'e' => 'ee',
+    'f' => 'ff',
+    'g' => 'gg',
+    'h' => 'hh',
+];
+
+foreach ($names as $key => $name) {
+    echo "{$name} <br>";
+}
