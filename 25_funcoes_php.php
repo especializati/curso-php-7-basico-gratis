8f62e7bc06f158a994c1f1f096683a8d92a93525
Carlos Ferreira <conta@especializati.com.br>
2018-03-20 09:40:16 -0300

    Manipulação de Strings no PHP (strtoupper,strtolower,ucfirst,ucwords)

---
+++ 25_funcoes_php.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = 'EspecializaTi - Cursos Online de TI';
+
+echo strtoupper($name);
+echo '<hr>';
+echo strtolower($name);
+echo '<hr>';
+echo ucfirst(strtolower($name));
+echo '<hr>';
+echo ucwords(strtolower($name));
