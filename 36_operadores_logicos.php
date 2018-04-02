318032a440a5af36a11106c9cbe593abe7e19adf
Carlos Ferreira <conta@especializati.com.br>
2018-04-02 14:21:41 -0300

    Operadores Lógicos no PHP

---
+++ 36_operadores_logicos.php
@@ -0,0 +1,46 @@
+<?php
+
+$a = 2;
+$b = 2;
+
+if (!$a != 2) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+}
+/**
+if (($a == $b && $a == 2) || $b == 2) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+}
+*/
+
+/**
+if ($a == $b || $a == 2) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+}
+ */
+/**
+if ($a == $b && $a == 2) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+}
+ */
+/**
+if ($a != $b OR $a == 3) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+}
+ */
+/**
+if ($a == $b AND $a == 3) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+}
+*/
