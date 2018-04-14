b9cc6b9e091cd5f2c0c04c70062aebdde55c15a2
Carlos Ferreira <conta@especializati.com.br>
2018-04-14 10:50:13 -0300

    Manipulação de Diretórios com PHP

---
+++ directories/directory.php
@@ -0,0 +1,11 @@
+<?php
+
+// var_dump(is_dir('teste'));
+// var_dump(is_dir('test'));
+// var_dump(mkdir('uploads'));
+// var_dump(mkdir('uploads/teste'));
+// var_dump(rmdir('uploads'));
+// var_dump(getcwd());
+// var_dump(scandir('..'));
+// echo '<pre>';
+// var_dump(glob('../*.php'));
