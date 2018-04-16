4bcd6a49b4017389e9c4ded4f306a47c9b45eaa9
Carlos Ferreira <conta@especializati.com.br>
2018-04-16 11:22:29 -0300

    Manipulação de arquivos no PHP (touch, copy, rename, unlink, file_exists)

---
+++ files/01_manipulacao_arquivos.php
@@ -0,0 +1,7 @@
+<?php
+
+// var_dump(touch('test.txt'));
+// var_dump(rename('test.txt', 'test_2.txt'));
+// var_dump(file_exists('test_2.txt'));
+// var_dump(copy('test_2.txt', 'test.txt'));
+// var_dump(unlink('test_2.txt'));
